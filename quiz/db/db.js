5edc7d6ca79714199608807ed0e1f520179cdacc
kkh1902 <kkh1902@naver.com>
2022-08-14 19:04:54 +0900

    quiz.ejs, quiz route변환 connection->pool , 사용자 response 값 insert

--- quiz/db/db.js
+++ quiz/db/db.js
@@ -1,24 +1,13 @@
-const mysql      = require('mysql');
-const mysql2 = require('mysql2/promise')
+const mysql = require("mysql");
 
 const connection = mysql.createConnection({
-  host     : '15.165.221.104',
-  port     : '54573',
-  user     : 'root',
-  password : '11111111',
-  database : 'quiz'
+    host: "15.165.221.104",
+    port: "54573",
+    user: "root",
+    password: "11111111",
+    database: "quiz",
 });
-
-const pool = mysql2.createPool({
-  host     : '15.165.221.104',
-  port     : '54573',
-  user     : 'root',
-  password : '11111111',
-  database : 'quiz'
-});
-
 
 connection.connect();
 
-module.exports = pool;
 module.exports = connection;
--- quiz/routes/quiz.js
+++ quiz/routes/quiz.js
@@ -1,95 +1,149 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var connection = require('../db/db');
+var connection = require("../db/db");
+var pool = require("../db/db2");
 
-router.get('/today', function(req, res, next) {
-  var today = new Date();
-  var year = today.getFullYear();
-  var month = today.getMonth() + 1;
-  var day = today.getDate();
-  var date = year + "-" + month + "-" + day;
-  if(req.session.uid) {
-    connection.query(
-      "SELECT date FROM res_quiz_today WHERE uid = ? AND date = ?;",
-      [ req.session.uid, date ],
-      (err1, res1, fld1) => {
+// router.get("/today", function (req, res, next) {
+//     var today = new Date();
+//     var year = today.getFullYear();
+//     var month = today.getMonth() + 1;
+//     var day = today.getDate();
+//     var date = year + "-" + month + "-" + day;
+//     if (req.session.uid) {
+//         connection.query(
+//             "SELECT date FROM res_quiz_today WHERE uid = ? AND date = ?;",
+//             [req.session.uid, date],
+//             (err1, res1, fld1) => {
+//                 try {
+//                     if (res1.length == 0) {
+//                         req.session.qid = [0, 0, 0, 0, 0];
+//                         req.session.aid = [0, 0, 0, 0, 0];
+//                         res.redirect("today/1");
+//                     } else {
+//                         res.send(
+//                             "<script>location.href='/'; alert('이미 오늘의 퀴즈를 푸셨습니다..');</script>"
+//                         );
+//                     }
+//                 } catch (err1) {
+//                     throw err1;
+//                 }
+//             }
+//         );
+//     } else {
+//         res.render("signin", {
+//             quizStates: quizStates,
+//             title: "로그인",
+//             signinState: false,
+//         });
+//     }
+// });
+
+router.get("/today", async (req, res) => {
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = today.getMonth() + 1;
+    var day = today.getDate();
+    var date = year + "-" + month + "-" + day;
+    if (req.session.uid) {
+        const res1 = await pool.query(
+            "SELECT date FROM res_quiz_today WHERE uid = ? AND date = ?;",
+            [req.session.uid, date]
+        );
         try {
-          if(res1.length == 0) {
-            req.session.qid = [0,0,0,0,0];
-            req.session.aid = [0,0,0,0,0];
-            res.redirect('today/1');
-          } else {
-            res.send(
-              "<script>location.href='/'; alert('이미 오늘의 퀴즈를 푸셨습니다..');</script>"
-            );
-          }
-        } catch (err1) {
-          throw err1;
+            if (res1.length - 2 == 0) {
+                req.session.qid = [0, 0, 0, 0, 0, 0]; //문제를 6개 만들고
+                req.session.aid = [0, 0, 0, 0, 0, 0]; //답도 6개 만든다.
+                res.redirect("today/1");
+            } else {
+                res.send(
+                    "<script>location.href='/'; alert('이미 오늘의 퀴즈를 푸셨습니다..');</script>"
+                );
+            }
+        } catch (error) {
+            console.log(error);
         }
-      }
-    );
-    
-  } else {
-    
-    res.render('signin', { quizStates: quizStates,  title: '로그인', signinState: false });
-  }
+    } else {
+        res.render("signin", {
+            quizStates: quizStates,
+            title: "로그인",
+            signinState: false,
+        });
+    }
 });
 
-router.get('/today/:id', function(req, res, next) {
-  var quizStates = {
-    qid: parseInt(req.params.id),
-  };
-  if(req.session.uid) {
-    res.render('quiz-today', { quizStates: quizStates,  title: '오늘의 퀴즈', signinState: true });
-  } else {
-    res.render('signin', { quizStates: quizStates,  title: '로그인', signinState: false });
-  }
+router.get("/today/:id", function (req, res, next) {
+    var quizStates = {
+        qid: parseInt(req.params.id),
+    };
+    console.log(quizStates);
+    if (req.session.uid) {
+        res.render("quiz-today", {
+            quizStates: quizStates,
+            title: "오늘의 퀴즈",
+            signinState: true,
+        });
+    } else {
+        res.render("signin", {
+            quizStates: quizStates,
+            title: "로그인",
+            signinState: false,
+        });
+    }
 });
 
-router.post('/today/:id', function(req, res, next) {
-  req.session.qid[req.params.id-1] = req.params.id;
-  req.session.aid[req.params.id-1] = req.body.quiz;
-  if(req.params.id == 5) {
-    res.redirect('/quiz/today-complete');
-  } else {
-    res.redirect('/quiz/today/'+(parseInt(req.params.id)+1));
-  }
+router.post("/today/:id", function (req, res, next) {
+    req.session.qid[req.params.id - 1] = req.params.id;
+    req.session.aid[req.params.id - 1] = req.body.quiz;
+    if (req.params.id == 6) {
+        res.redirect("/quiz/today-complete");
+    } else {
+        res.redirect("/quiz/today/" + (parseInt(req.params.id) + 1));
+    }
 });
 
-router.get('/today-complete', function(req, res, next) {
-  var today = new Date();
-  var year = today.getFullYear();
-  var month = today.getMonth() + 1;
-  var day = today.getDate();
-  var date = year + "-" + month + "-" + day;
-  var uid = req.session.uid;
-  var qid = req.session.qid;
-  var aid = req.session.aid;
-  if(req.session.uid) {
-    connection.query(
-      "INSERT INTO res_quiz_today VALUES (null, ?, ?, 0);",
-      [ uid, date ],
-      (err1, res1, fld1) => {
+router.get("/today-complete", async (req, res) => {
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = today.getMonth() + 1;
+    var day = today.getDate();
+    var date = year + "-" + month + "-" + day;
+    var uid = req.session.uid;
+    var qid = req.session.qid; // 문제 번호
+    var aid = req.session.aid; // 사용자 답
+    console.log(aid);
+    if (req.session.uid) {
+        const user_res = await pool.query(
+            "INSERT INTO res_quiz_today VALUES (null, ?, ?, ?, ?, ?, ? , ?, ?);",
+            [uid, date, aid[0], aid[1], aid[2], aid[3], aid[4], aid[5]]
+        );
         try {
-          res.render('quiz-today-complete', { title: '오늘의 퀴즈 완료', signinState: true });
-        } catch(err1) {
-          throw err1;
+            res.render("quiz-today-complete", {
+                title: "오늘의 퀴즈 완료",
+                signinState: true,
+            });
+        } catch (error) {
+            console.log(error);
         }
-      }
-    );
-  } else {
-    res.redirect('/signin');
-  }
+    } else {
+        res.redirect("/signin");
+    }
 });
 
-router.get('/battle', function(req, res, next) {
-  var signinStates = {
-  };
-  if(req.session.uid) {
-    res.render('quiz-battle', { signinStates: signinStates,  title: '오늘의 퀴즈', signinState: true });
-  } else {
-    res.render('signin', { signinStates: signinStates,  title: '로그인', signinState: false });
-  }
+router.get("/battle", function (req, res, next) {
+    var signinStates = {};
+    if (req.session.uid) {
+        res.render("quiz-battle", {
+            signinStates: signinStates,
+            title: "오늘의 퀴즈",
+            signinState: true,
+        });
+    } else {
+        res.render("signin", {
+            signinStates: signinStates,
+            title: "로그인",
+            signinState: false,
+        });
+    }
 });
 
 module.exports = router;
